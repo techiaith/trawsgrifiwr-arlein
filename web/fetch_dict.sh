4668f7b5a4cb8423620b5d0177c78e50a9d94a22
Stephen Russell <str20tbl@bangor.ac.uk>
2021-03-12 07:32:06 +0000

    sgriptiau i nol modelau iaith newydd

--- web/fetch_dict.sh
+++ web/fetch_dict.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+while getopts ":r:" arg; do
+  case $arg in
+    r) RELEASE=$OPTARG;;
+  esac
+done
+if [[ RELEASE == "" ]]; then
+  echo "please provide a release tag -r 20.03"
+  exit 0
+fi
 echo "fetching language models"
-wget https://github.com/techiaith/docker-deepspeech-cy/releases/download/21.01/techiaith_bangor_21.01.pbmm -o app/transcriber/models/lm.pbmm
-wget https://github.com/techiaith/docker-deepspeech-cy/releases/download/21.01/techiaith_bangor_transcribe_21.01.scorer -o app/transcriber/models/lm.scorer
+wget https://github.com/techiaith/docker-deepspeech-cy/releases/download/$RELEASE/techiaith_bangor_$RELEASE.pbmm -o app/transcriber/models/lm.pbmm
+wget https://github.com/techiaith/docker-deepspeech-cy/releases/download/$RELEASE/techiaith_bangor_transcribe_$RELEASE.scorer -o app/transcriber/models/lm.scorer
--- web/init.sh
+++ web/init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -f "web/app/transcriber/models/lm.pbmm" "web/app/transcriber/models/lm.scorer" ]; then \
+  echo "no language models, fetching..."
+  wget https://github.com/techiaith/docker-deepspeech-cy/releases/download/21.03/techiaith_bangor_21.03.pbmm -O app/transcriber/models/lm.pbmm
+  wget https://github.com/techiaith/docker-deepspeech-cy/releases/download/21.03/techiaith_bangor_transcribe_21.03.scorer -O app/transcriber/models/lm.scorer
+fi
 echo "pip install -e ."
 pip install -e . >> docker-build.log
 mkdir -p app/alembic/versions
